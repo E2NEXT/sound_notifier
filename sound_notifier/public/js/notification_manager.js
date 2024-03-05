d5623e2a16b019ed35cba436fa7d67c7433866ba
osama1998H <osamamuhammed555@gmail.com>
2024-03-05 11:04:48 +0300

    refactor the code and make the last notification fields readonly

--- sound_notifier/public/js/notification_manager.js
+++ sound_notifier/public/js/notification_manager.js
@@ -1,61 +1,72 @@
 
 $(document).ready(() => {
-  frappe.realtime.on('notification', function (data) {
-    var current_user = frappe.session.user;
-    frappe.db.get_list(
-        'Notification Log', {
-        filters: {
-            "for_user": current_user,
-        }, fields: ['subject', 'creation', 'name'],
-        order_by: 'creation desc',
-        limit: 1 
-    }
-    ).then(function (value) {
-        if (value.length > 0) {
-            frappe.db.get_list(
-                'Last Notification', {
-                filters: {
-                    "user": current_user,
-                }, fields: ['notification_log', 'name'],
-                limit: 1
-            }
-            ).then(function (value2) {
+    frappe.realtime.on('notification', async () => {
+        const currentUser = frappe.session.user;
 
-                if (value2.length > 0) {
-                    if (value2[0]['notification_log'] != value[0]['name']) {
-                        frappe.show_alert({
-                            message: "New Notification : " + value[0]['subject'],
-                            indicator: 'green'
-                        }); var audio = new Audio('/assets/sound_notifier/sounds/1.mp3');
-                        audio.play();
-                        frappe.db.set_value('Last Notification', value2[0]['name'], 'notification_log', value[0]['name']);
-                    }
-                } else {
-                    frappe.call({
-                        method: 'frappe.client.insert',
-                        args: {
-                            doc: {
-                                doctype: 'Last Notification',
-                                user: current_user,
-                                notification_log: value[0]['name']
-                            }
-                        },
-                        callback: function (response) {
-                            if (!response.exc) {
-                                frappe.show_alert({
-                                    message:"New Notification : " +  value[0]['subject'],
-                                    indicator: 'green'
-                                });
-                                var audio = new Audio('/assets/sound_notifier/sounds/1.mp3');
-                                audio.play();
-                            }
-                        }
-                    });
+        try {
+            const latestNotification = await getLatestNotification(currentUser);
+
+            if (latestNotification) {
+                const lastNotified = await getLastNotified(currentUser);
+
+                if (!lastNotified || lastNotified.notification_log !== latestNotification.name) {
+                    showNewNotificationAlert(latestNotification.subject);
+                    playNotificationSound();
+                    updateLastNotification(currentUser, latestNotification.name, lastNotified?.name);
                 }
-
-            });
-
+            }
+        } catch (error) {
+            console.error("Error handling notification:", error);
         }
     });
-  });
-})
+});
+
+async function getLatestNotification(user) {
+    const notifications = await frappe.db.get_list('Notification Log', {
+        filters: { "for_user": user },
+        fields: ['subject', 'creation', 'name'],
+        order_by: 'creation desc',
+        limit: 1
+    });
+
+    return notifications.length > 0 ? notifications[0] : null;
+}
+
+async function getLastNotified(user) {
+    const notifications = await frappe.db.get_list('Last Notification', {
+        filters: { "user": user },
+        fields: ['notification_log', 'name'],
+        limit: 1
+    });
+
+    return notifications.length > 0 ? notifications[0] : null;
+}
+
+function showNewNotificationAlert(subject) {
+    frappe.show_alert({
+        message: `New Notification : ${subject}`,
+        indicator: 'green'
+    });
+}
+
+function playNotificationSound() {
+    const audio = new Audio('/assets/sound_notifier/sounds/1.mp3');
+    audio.play();
+}
+
+async function updateLastNotification(user, notificationLogName, lastNotificationName) {
+    if (lastNotificationName) {
+        await frappe.db.set_value('Last Notification', lastNotificationName, 'notification_log', notificationLogName);
+    } else {
+        await frappe.call({
+            method: 'frappe.client.insert',
+            args: {
+                doc: {
+                    doctype: 'Last Notification',
+                    user: user,
+                    notification_log: notificationLogName
+                }
+            }
+        });
+    }
+}
